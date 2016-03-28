4466cb84645c2313bd5d75c6bfd15ebf39f01b7a
Sander Rõuk <sander@phantomarts.ee>
2016-03-28 19:56:05 +0300

    Cleaning of GIT

--- backend/connect.php
+++ backend/connect.php
@@ -1,6 +1,6 @@
 <?php
 function establish(){
-    require_once __DIR__.'/../config.php';
+    require_once 'config.php';
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     if($conn->connect_error){
         return $conn->connect_error;
