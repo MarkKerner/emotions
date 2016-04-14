69129dac1ddf84f797601617f14a62863f327259
Mark Kerner <mark.kerner@hotmail.com>
2016-04-14 13:57:49 +0300

    Login and profile views now change dynamically.

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -2,11 +2,31 @@ import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
 import {ProfileComponent} from './profile.component';
+import {LoginComponent} from './login.component';
+
+
+
+@RouteConfig([{
+	path: '/profile',
+	name: 'Profile',
+		component: ProfileComponent,
+		useAsDefault: true
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: LoginComponent
+	}
+])
 
 @Component({
     selector: 'feel-app',
     templateUrl: 'app/html/app.component.html',
     styleUrls: ['app/css/app.component.css'],
-	directives: [ProfileComponent]
+	directives: [ROUTER_DIRECTIVES],
+	providers: [
+		ROUTER_PROVIDERS
+	]
 })
+
 export class AppComponent { }
---
+++ frontend/app/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'login-component',
+    templateUrl: 'app/html/login.component.html',
+    styleUrls: ['app/css/login.component.css']
+})
+export class LoginComponent { }
