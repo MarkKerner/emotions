8b11e5762067cc91ac51e76564f8a5c3f19ac3ea
Mark Kerner <mark.kerner@hotmail.com>
2016-05-08 01:25:36 +0300

    Frontend clean

--- frontend/app/login.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from 'angular2/core';
-
-@Component({
-    selector: 'login-component',
-    templateUrl: 'app/html/login.component.html',
-    styleUrls: ['app/css/login.component.css']
-})
-export class LoginComponent { }
--- frontend/app/profile.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from 'angular2/core';
-
-@Component({
-    selector: 'profile-component',
-    templateUrl: 'app/html/profile.component.html',
-    styleUrls: ['app/css/profile.component.css']
-})
-export class ProfileComponent { }
